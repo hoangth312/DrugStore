5d86a88a635f8c09ed417e3d590b70c1e2a699e5
toquocphong0307 <toquocphong0307@users.noreply.github.com>
2020-12-28 14:48:33 +0700

    add lan 3

---
+++ app/src/main/java/com/hoanganh/drugstore/InformationOfClinicFragment.kt
@@ -0,0 +1,15 @@
+package com.hoanganh.drugstore
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class InformationOfClinicFragment :Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        var v = inflater.inflate(R.layout.fragment_information_of_clinic, container, false)
+        return v
+
+    }
+}
