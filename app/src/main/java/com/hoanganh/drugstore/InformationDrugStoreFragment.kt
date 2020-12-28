de670063b3d6996750b5fa3ef0049213ee6b9683
hoangth312 <hoangth.312@gmail.com>
2020-12-28 16:40:58 +0700

    conmit lan 3

---
+++ app/src/main/java/com/hoanganh/drugstore/InformationDrugStoreFragment.kt
@@ -0,0 +1,17 @@
+package com.hoanganh.drugstore
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class InformationDrugStoreFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        var v = inflater.inflate(R.layout.fragment_information_drug_store, container, false)
+        return v
+//        return v
+
+    }
+
+}
