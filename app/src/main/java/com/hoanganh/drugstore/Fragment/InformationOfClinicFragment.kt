9ac37e18f8c004d64990fc3c427b6a9411dce203
hoangth312 <hoangth.312@gmail.com>
2021-01-12 09:27:07 +0700

    hoang anh lan 2

--- app/src/main/java/com/hoanganh/drugstore/Fragment/InformationOfClinicFragment.kt
+++ app/src/main/java/com/hoanganh/drugstore/Fragment/InformationOfClinicFragment.kt
@@ -4,14 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.hoanganh.drugstore.R
+import kotlinx.android.synthetic.main.app_bar_fragments.view.*
+
 
 class InformationOfClinicFragment :Fragment() {
     lateinit var viewOfLayout: View
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         viewOfLayout = inflater.inflate(R.layout.fragment_information_of_clinic, container, false)
+
+        viewOfLayout.toolbar_title.text = getString(R.string.detailOfClinic)
         return viewOfLayout
+
+    }
+    override fun onResume() {
+        super.onResume()
+        (activity as AppCompatActivity?)!!.supportActionBar!!.hide()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        (activity as AppCompatActivity?)!!.supportActionBar!!.show()
 
     }
 }
