7a6f28f86f35319f3b7d1c487f37b3a4c549c6d2
hoangth312 <hoangth.312@gmail.com>
2021-01-18 17:34:37 +0700

    lan 218/1

--- app/src/main/java/com/hoanganh/drugstore/Model/SearchDrugsModel.kt
+++
@@ -1,7 +0,0 @@
-package com.hoanganh.drugstore.Model
-
-class SearchDrugsModel(var image:Int, var name: String,
-                       var subTilte: String,
-                       var description: String,
-                       var numLike: Int,
-                       var numCmt: Int)
